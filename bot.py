86324534f4c8c3ed24d9a39bd093e7d53ed4b84d
Matt Chan <matt.chan120@gmail.com>
2023-04-11 10:27:21 -0400

    bot with basic functionality, please create discord bot and input your own token in the bot file

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import responses
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+        
+
+def run_discord_bot():
+    TOKEN = 'MTA4NDU2MTQ0NjY4OTU3MDkxNw.GpCIFn._M3Q-kja-VrRRSGK8VWBKYRPFD6En-g2VjTpYQ'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is online')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" {{channel}}')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    @client.event
+    async def on_raw_reaction_add(payload):
+        emoji_id = payload.emoji.name
+        print(emoji_id)
+
+    @client.event
+    async def on_raw_reaction_remove(payload):
+        print('removal noted')
+
+    client.run(TOKEN)
